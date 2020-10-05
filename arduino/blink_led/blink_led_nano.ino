929b7edc9c3cceecc871b66cf39441c8b3ccdca2
js899 <49071268+js899@users.noreply.github.com>
2020-10-05 16:53:47 +0530

    Update blink_led_nano.ino

--- arduino/blink_led/blink_led_nano.ino
+++ arduino/blink_led/blink_led_nano.ino
@@ -1,11 +1,11 @@
-const int LED = 13;    // connected LED on Pin 13, named Pin 13 as LED
+const int LED = 13;    // connected LED on Digital OUTPUT Pin no. 13, named it as LED.
 
-void setup() 
+void setup()    // setup function that runs once as soon as the program starts running.
 {                
-    pinMode(LED, OUTPUT);    // defineing LED pin as O/P Pin.   
+    pinMode(LED, OUTPUT);    // defining LED pin for OUTPUT.
 }
 
-void loop()
+void loop()    // keeps running (or until a condition is met). It actively controls the Arduino board.
 {
     digitalWrite(LED, HIGH);   // turns LED ON
     delay(1000);               // stays ON for a second
